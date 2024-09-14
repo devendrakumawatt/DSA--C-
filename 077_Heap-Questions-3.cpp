84c1bdaff2d797ed61de5bdf4486969d251b861f
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-09-14 16:51:24 +0000

    Heap Interview Questions - III

--- 077_Heap-Questions-3.cpp
+++ 077_Heap-Questions-3.cpp
@@ -4,6 +4,8 @@
 #include<queue>
 using namespace std;
 
+ 							// *** Smallest Range in k-list ***
+
 class Node {
 public:
 	int data;
@@ -83,3 +85,95 @@ int main(){
 
     cout << "Smallest range is of size: " << kSorted(a, k, n) << endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+/*							// *** Median in a Stream ***
+
+
+int signum(int a, int b){
+	if(a == b){
+		return 0;
+	}
+	else if(a > b){
+		return 1;
+	}
+	else{
+		return -1;
+	}
+}
+
+void callMedian(int element, priority_queue<int> &maxi, priority_queue<int, vector<int>, greater<int>> &mini , int &median){
+	switch(signum(maxi.size(), mini.size())){
+
+		case 0: if(element > median){
+					mini.push(element);
+					median = mini.top();
+				}
+				else{
+					maxi.push(element);
+					median = maxi.top();
+				}
+				break;
+
+		case 1: if(element > median){
+					mini.push(element);
+					median = (mini.top() + maxi.top())/2;
+				}
+				else{
+					mini.push(maxi.top());
+					maxi.pop();
+					maxi.push(element);
+					median = (mini.top() + maxi.top())/2;
+				}
+				break;
+
+		case -1: if(element > median){
+					maxi.push(mini.top());
+					mini.pop();
+					mini.push(element);
+					median = (mini.top() + maxi.top())/2;
+				}
+				else{
+					maxi.push(element);
+					median = (mini.top() + maxi.top())/2;
+				}
+				break;
+	}
+
+}
+
+
+vector<int> findMedian(vector<int> &arr, int n){
+	vector<int> ans;
+	priority_queue<int> maxheap;
+	priority_queue<int, vector<int>, greater<int>> minHeap;
+	int median = 0;
+
+	for(int i=0; i<n; i++){
+		callMedian(arr[i], maxheap, minHeap, median);
+		ans.push_back(median);
+	}
+	return ans;
+}
+
+int main(){
+	vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+    int n = arr.size();
+    vector<int> medians = findMedian(arr, n);
+
+    cout << "Running medians: ";
+    for (int m : medians) {
+        cout << m << " ";
+    }
+    cout << endl;
+}
+*/
