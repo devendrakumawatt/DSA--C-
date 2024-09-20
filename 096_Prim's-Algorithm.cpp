5b9a194ae175ba1c81551fcf330ce31954989bc6
Devendra Kumawat <149409257+devendrakumawatt@users.noreply.github.com>
2024-09-20 17:20:51 +0000

    Minimum Spanning Tree using Prim's Algorithm

---
+++ 096_Prim's-Algorithm.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<list>
+#include<limits.h>
+using namespace std;
+
+vector<pair<pair<int,int>, int>> calculatePrismMST(int n, int m, vector<pair<pair<int,int>, int>> &g){
+
+	// create adj list
+	unordered_map<int, list<pair<int,int>>> adj;
+	for(int i=0; i<g.size(); i++){
+		int u = g[i].first.first;
+		int v = g[i].first.second;
+
+		int w = g[i].second;
+
+		adj[u].push_back(make_pair(v,w));
+		adj[v].push_back(make_pair(u,w));
+	}
+
+	vector<int> key(n+1);
+	vector<bool> mst(n+1);
+	vector<int> parent(n+1);
+
+	for(int i=0; i<=n; i++){
+		key[i] = INT_MAX;
+		parent[i] = -1;
+		mst[i] = false;
+	}
+
+	// let's start the algo
+	key[1] = 0;
+	parent[1] = -1;
+
+	for(int i=1; i<=n; i++){
+
+		int mini = INT_MAX;
+		int u;
+
+		// finding the mini node
+		for(int v=1; v<=n; v++){
+			if(mst[v] == false && key[v] < mini){
+				u = v;
+				mini = key[v];
+			}
+		}
+
+		// mark mini node as true
+		mst[u] = true;
+
+		// check adj nodes
+		for(auto it: adj[u]){
+			int v = it.first;
+			int w = it.second;
+			if(mst[v] == false && w < key[v]){
+				parent[v] = u;
+				key[v] = w;
+			}
+		}
+	}
+
+	vector<pair<pair<int,int>, int>> result;
+	for(int i=2; i<=n; i++){
+		result.push_back({{parent[i], i}, key[i]});
+	}
+	return result;
+}
+
+int main(){
+	int n = 5;
+    int m = 6;
+
+    vector<pair<pair<int, int>, int>> g = {
+        {{0, 1}, 2},
+        {{0, 3}, 6},
+        {{1, 3}, 8},
+        {{1, 4}, 5},
+        {{1, 2}, 3},
+        {{2, 4}, 7}
+    };
+
+    auto mst = calculatePrismMST(n, m, g);
+
+    cout << "Edges in the Minimum Spanning Tree (MST):" << endl;
+    for(auto edge : mst){
+        cout << edge.first.first << " -- " << edge.first.second << " == " << edge.second << endl;
+    }
+}
